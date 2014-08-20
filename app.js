e33817b0e3cae17a3852491c4c703482426b2a5c
hwclass <harderworking.class@gmail.com>
2014-08-20 22:23:32 +0300

    Modified the context.views, added model layer.

--- app.js
+++ app.js
@@ -2,7 +2,25 @@
 var app = (function () {
     
     var context = {
-      views : [] 
+      views : [],
+      models : [
+        {
+          dataLayer : function(args) {
+            this.testVar1 = args.testVar1;
+            this.testVar2 = args.testVar2;
+            this.testVar3 = args.testVar3;
+            return this;
+          }
+        },
+        {
+          google_tag_params : function(args) {
+            this.testVar1 = args.testVar1;
+            this.testVar2 = args.testVar2;
+            this.testVar3 = args.testVar3;
+            return this;
+          }
+        }
+      ]
     }
     
     var topics = {};
@@ -15,9 +33,26 @@ var app = (function () {
     var getContext = function (view) {
       var currentView;
       for (var index in context.views) {
-        return context.views[index][view];
+        if (context.views[index].config.title == view) {
+          currentView = context.views[index];
+        };
       }
       return currentView;
+    }
+
+    var removeContext = function(title) {
+      /*
+      for (var count=0, len=context.views.length; count < len; count++) {        
+        if (context.views[count].config.title == title) {
+          delete context.views[count][title];
+        }
+      }
+      */
+      for (var index in context.views) {
+        if (context.views[index]) {
+          console.log(context.views[index][title].config);
+        }
+      }
     }
     
     var subscribe = function(topic, listener) {
@@ -49,15 +84,16 @@ var app = (function () {
     }
 
     var init = function() {
-      this.publish('customEventForFirstTestData', {testMessage : 'test message'});
-      this.publish('customEventForSecondTestData', {testMessage : 'test message'});
-      this.publish('customEventForThirdTestData', {testMessage : 'test message'});
+      this.publish('customEventForFirstTestData', {testMessage : 'base module initialized.'});
+      this.publish('customEventForSecondTestData', {testMessage : 'mainpage module initialized.'});
+      this.publish('customEventForThirdTestData', {testMessage : 'productlist module initialized.'});
     }
     
     return {
       context : context,
       define : defineContext,
       get : getContext,
+      remove : removeContext,
       subscribe : subscribe,
       publish : publish,
       init : init
