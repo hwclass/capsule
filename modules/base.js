06e8f3c8bede1bb475103ceb94d350e795c168ec
hwclass <harderworking.class@gmail.com>
2014-08-25 19:19:23 +0300

    Added comments.

--- modules/base.js
+++ modules/base.js
@@ -1,8 +1,17 @@
 /*Context : Base*/
+
 app.define('base', {
+  /**
+   * config{} contains configuration properties.
+   *
+  */
   config : {
     title : 'Base View'
   },
+  /**
+   * models{} contains module-specific model structures.
+   *
+  */
   models : [
     {
       dataLayer : function(args) {
@@ -21,9 +30,17 @@ app.define('base', {
       }
     }
   ],
+  /**
+   * el{} wraps the elements living in the context of current module.
+   *
+  */
   el : {
     container : document.querySelector('body')
   },
+  /**
+   * fn{} is used to reach module specific methods / functions.
+   *
+  */
   fn : {
     isUndefined : function (obj) {
       return (typeof obj === 'undefined');
@@ -32,9 +49,17 @@ app.define('base', {
       return (typeof obj === 'null');
     }
   },
+  /**
+   * getInstance() returns the current context.
+   *
+  */
   getInstance : function () {
   	return this;
   },
+  /**
+   * init() stars the current module.
+   *
+  */
   init : function () {
     app.subscribe('customEventForBaseTestData', function(args) {
       console.log(args);
